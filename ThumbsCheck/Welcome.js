c9e1eea2abf02cb118a9bf58582b07bdbef2e0f3
Lauren <lhspiegel@gmail.com>
2015-05-12 16:05:25 -0700

    Modify index page to provide routing and create welcome page with login button

---
+++ ThumbsCheck/Welcome.js
@@ -0,0 +1,71 @@
+
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+} = React;
+
+var Welcome = React.createClass({
+
+  login() {
+    console.log("here");
+  },
+
+
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Thumbs Check!
+        </Text>
+        <Text style={styles.instructions}>
+          Login through GitHub
+        </Text>
+        <Text style={styles.instructions}>
+          Get Voting
+        </Text>
+        <TouchableHighlight style={styles.button} onPress={this.login}
+
+            underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+});
+
+module.exports = Welcome;
