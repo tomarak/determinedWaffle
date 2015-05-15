c8498407f1abd093f0fcd0d03bc2111fbee9cb52
Brian <briancjliu@gmail.com>
2015-05-15 15:52:06 -0700

    Added randomizing Imgur images at thank you page

--- ThumbsCheck/Submitted.js
+++ ThumbsCheck/Submitted.js
@@ -32,20 +32,26 @@ var styles = StyleSheet.create({
     borderColor: "#76BD72",
   },
   image: {
-    // flex: 0.1,
     height: 200,
     width: 335
   },
 });
 
+
+var imgurArr = ['1cFZKU6.jpg', 'lpgzenr.jpg', 'kaSAiFO.jpg', 'O1u0Vad.jpg', '049aXNY.jpg', '0HUaYF9.gif',
+'Rk21lRI.jpg', 'Y1v6cAQ.jpg', 'PG9ELxT.jpg', 'q4jGueU.jpg', 'ii3cS7L.jpg', 'NHv4DA4.jpg', 'LTuB2HH.gifv',
+'wG4y01d.jpg', 'tpyiLGZ.jpg', 'AEQLyFV.jpg', 'zDZmbsA.jpg', 'SYWjy1l.jpg', 'VJirqYG.jpg', '4pKjgPg.jpg',
+'22Je1dz.jpg', 'FbiW7PP.jpg'];
+
 class Submitted extends Component{
+
   render(){
     return(
       <View style = {styles.container}>
         <Text style = {styles.description}>
           Thanks for Voting, {this.props.name}!
         </Text>
-        <Image source={require('image!PuppyFive')} style={styles.image}/>
+        <Image defaultSource={require('image!PuppyFive')} style={styles.image} source={{uri: 'http://i.imgur.com/' + imgurArr[Math.floor(Math.random() * imgurArr.length)]}}/>
       </View>
     );
   }
